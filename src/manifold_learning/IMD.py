f9996406893c16aa4358552181b5e5c8dcbb35d7
z7743 <kapikapycapybara@gmail.com>
2024-05-16 18:39:57 +0900

    Exclution radius and minor changes

--- src/manifold_learning/IMD.py
+++ src/manifold_learning/IMD.py
@@ -48,19 +48,18 @@ class LinearProjectionNDim(nn.Module):
 
 class IMD_nD:
 
-    def __init__(self, input_dim, embed_dim, n_components, learning_rate=0.001,device="cuda",optimizer="Adam",random_state=None):
+    def __init__(self, input_dim, embed_dim, n_components, subtract_corr=True, learning_rate=0.001,device="cuda",optimizer="Adam",random_state=None):
         self.device = device
 
         self.model = LinearProjectionNDim(input_dim, embed_dim, n_components, device,random_state)
 
         self.optimizer = getattr(optim, optimizer)(self.model.parameters(), lr=learning_rate,)
-        #self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate,)
-
+        self.subtract_corr = subtract_corr
         self.loss_history = []
 
-    def fit(self, X, sample_len, library_len, nbrs_num, tp, epochs=10, num_batches=32):
+    def fit(self, X, sample_len, library_len, exclusion_rad, nbrs_num, tp, epochs=10, num_batches=32):
 
-        X = torch.tensor(X,requires_grad=True, device="cpu", dtype=torch.float32)
+        X = torch.tensor(X,requires_grad=True, device=self.device, dtype=torch.float32)
         X_train, y_train = X[:X.shape[0]-tp], X[tp:]
 
         dataset = RandomSubsetDataset(X_train, y_train, sample_len, library_len, num_batches,device=self.device)
@@ -70,13 +69,13 @@ class IMD_nD:
             total_loss = 0
             self.optimizer.zero_grad()
 
-            for subset_X, subset_y, sample_X, sample_y in dataloader:
+            for subset_idx, sample_idx, subset_X, subset_y, sample_X, sample_y in dataloader:
                 subset_X_z = self.model(subset_X)
                 subset_y_z = self.model(subset_y)
                 sample_X_z = self.model(sample_X)
                 sample_y_z = self.model(sample_y)
 
-                loss = self.loss_fn(sample_X_z, sample_y_z, subset_X_z, subset_y_z, nbrs_num)
+                loss = self.loss_fn(subset_idx, sample_idx,sample_X_z, sample_y_z, subset_X_z, subset_y_z, nbrs_num, exclusion_rad)
                 loss /= num_batches
                 loss.backward()
                 total_loss += loss.item() 
@@ -93,28 +92,36 @@ class IMD_nD:
         return outputs.cpu().numpy()
 
 
-    def loss_fn(self, sample_td, sample_pred, subset_td, subset_pred, nbrs_num):
+    def loss_fn(self, subset_idx, sample_idx, sample_td, sample_pred, subset_td, subset_pred, nbrs_num, exclusion_rad):
         dim = sample_td.shape[-1]
-        corr = torch.abs(self.get_autoreg_matrix(sample_td, sample_pred))
-
-        ccm = torch.abs(self.get_ccm_matrix(sample_td, sample_pred, subset_td, subset_pred, nbrs_num))
+        ccm = torch.abs(self.get_ccm_matrix_approx(subset_idx, sample_idx, sample_td, sample_pred, subset_td, subset_pred, nbrs_num, exclusion_rad))
         mask = torch.eye(dim,dtype=bool,device=self.device)
-        if dim > 1:
-            score = 1 + (torch.mean(ccm[:,~mask].reshape(-1,dim,dim-1),axis=2)/2 + \
-                        torch.mean(ccm[:,~mask].reshape(-1,dim-1,dim),axis=1)/2 - \
-                    (ccm[:,mask]**2) \
-                    +(corr[:,mask]**2)
-                        ).mean()
-        else:
-            score = 1 + (-ccm[:,0,0] + corr[:,0,0]).mean()
-        return score
-    
 
-    def get_ccm_matrix(self, sample_td, sample_pred, subset_td, subset_pred, nbrs_num):
+        if self.subtract_corr:
+            corr = torch.abs(self.get_autoreg_matrix_approx(sample_td, sample_pred))
+            if dim > 1:
+                score = 1 + (torch.mean(ccm[:,~mask].reshape(-1,dim,dim-1),axis=2)/2 + \
+                            torch.mean(ccm[:,~mask].reshape(-1,dim-1,dim),axis=1)/2 - \
+                        (ccm[:,mask]**2) \
+                        +(corr[:,mask]**2)
+                            ).mean()
+            else:
+                score = 1 + (-ccm[:,0,0] + corr[:,0,0]).mean()
+            return score
+        else:
+            if dim > 1:
+                score = 1 + (torch.mean(ccm[:,~mask].reshape(-1,dim,dim-1),axis=2)/2 + \
+                            torch.mean(ccm[:,~mask].reshape(-1,dim-1,dim),axis=1)/2 - \
+                        (ccm[:,mask]**2)).mean()
+            else:
+                score = 1 + (-ccm[:,0,0]).mean()
+            return score
+
+    def get_ccm_matrix_approx(self, subset_idx, sample_idx, sample_td, sample_pred, subset_td, subset_pred, nbrs_num, exclusion_rad):
         dim = sample_td.shape[-1]
         E = sample_td.shape[-2]
         
-        indices = self.get_nbrs_indices(torch.permute(subset_td,(2,0,1)),torch.permute(sample_td,(2,0,1)), nbrs_num)
+        indices = self.get_nbrs_indices(torch.permute(subset_td,(2,0,1)),torch.permute(sample_td,(2,0,1)), nbrs_num, subset_idx, sample_idx, exclusion_rad)
         I = indices.reshape(dim,-1).T 
         
         subset_pred_indexed = subset_pred[I[:, None,None, :],torch.arange(E,device=self.device)[:,None,None], torch.arange(dim,device=self.device)[None,:,None]]
@@ -125,7 +132,7 @@ class IMD_nD:
         r_AB = self.get_batch_corr(A,B)
         return r_AB
     
-    def get_autoreg_matrix(self, A, B):
+    def get_autoreg_matrix_approx(self, A, B):
         dim = A.shape[-1]
         E = A.shape[-2]
         
@@ -146,17 +153,27 @@ class IMD_nD:
         r_AB = sum_AB / torch.sqrt(sum_AA * sum_BB)
         return r_AB
 
-    def get_nbrs_indices(self, lib, sublib, n_nbrs):
+    def get_nbrs_indices(self, lib, sublib, n_nbrs, subset_idx, sample_idx, exclusion_rad):
         dist = torch.cdist(sublib,lib)
-        indices = torch.topk(dist, n_nbrs, largest=False)[1]
-        return indices
+        indices = torch.topk(dist, n_nbrs + 2*exclusion_rad, largest=False)[1]
+        if exclusion_rad > 0:
+            
+            mask = ~((subset_idx[0][indices] <= sample_idx[0][:,None]+exclusion_rad) & (subset_idx[0][indices] >= sample_idx[0][:,None]-exclusion_rad))
+            cumsum_mask = mask.cumsum(dim=2)
+            selector = cumsum_mask <= n_nbrs
+            selector = selector * mask
+            
+            indices_exc = indices[selector].view(mask.shape[0],mask.shape[1],n_nbrs)
+            return indices_exc
+        else:
+            return indices
 
     def get_loss_history(self):
         return self.loss_history
     
 
 class RandomSubsetDataset(Dataset):
-    def __init__(self, X, y, sample_len, library_len, num_batches, device="cuda"):
+    def __init__(self, X, y, sample_len, library_len, num_batches=32, device="cuda"):
         """
         Initializes the RandomSubsetDataset.
         
@@ -179,14 +196,14 @@ class RandomSubsetDataset(Dataset):
     def __len__(self):
         return self.num_batches #Temporary solution to sample number of samples
     
-    def p__getitem__(self, idx):
+    def __getitem__(self, idx):
         sample_idx = torch.argsort(torch.rand(self.num_datapoints,device=self.device))[0:self.sample_len]
-        library_idx = torch.argsort(torch.rand(self.num_datapoints,device=self.device))[0:self.library_len+self.sample_len]
+        library_idx = torch.argsort(torch.rand(self.num_datapoints,device=self.device))[0:self.library_len + self.sample_len]
         library_idx = library_idx[(library_idx.view(1, -1) != sample_idx.view(-1, 1)).all(dim=0)][0:self.library_len]
         
-        return self.X[library_idx],self.y[library_idx], self.X[sample_idx], self.y[sample_idx]
+        return library_idx, sample_idx, self.X[library_idx],self.y[library_idx], self.X[sample_idx], self.y[sample_idx]
     
-    def __getitem__(self, idx):
+    def p__getitem__(self, idx):
         sample_idx = torch.argsort(torch.rand(self.num_datapoints))[0:self.sample_len]
         library_idx = torch.argsort(torch.rand(self.num_datapoints))[0:self.library_len+self.sample_len]
         library_idx = library_idx[(library_idx.view(1, -1) != sample_idx.view(-1, 1)).all(dim=0)][0:self.library_len]
--- src/manifold_learning/data/data_loader.py
+++ src/manifold_learning/data/data_loader.py
@@ -11,3 +11,35 @@ def load_csv_dataset(filename: str) -> pd.DataFrame:
     if not os.path.exists(filepath):
         raise FileNotFoundError(f"File '{filename}' not found in datasets directory.")
     return pd.read_csv(filepath)
+
+def load_ld2011_2014_dataset() -> pd.DataFrame:
+    filename = "LD2011_2014.txt"
+    filepath = os.path.join(DATA_DIR, filename)
+    
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f"File '{filename}' not found in datasets directory.")
+    
+    # Load the CSV file with pandas, specifying the delimiter and avoiding DtypeWarning
+    df = pd.read_csv(filepath, sep=';', low_memory=False)
+    
+    # Replace commas with dots for numeric conversion
+    df = df.apply(lambda x: x.str.replace(',', '.') if x.dtype == 'object' else x)
+    
+    # Convert all columns to numeric, setting invalid parsing to NaN
+    for col in df.columns:
+        try:
+            df[col] = pd.to_numeric(df[col])
+        except ValueError:
+            pass
+    
+    # Replace NaNs with zero if needed
+    df.fillna(0, inplace=True)
+    
+    return df
+
+def load_traffic_dataset() -> pd.DataFrame:
+    filename = "train_1.csv"
+    filepath = os.path.join(DATA_DIR, filename)
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f"File '{filename}' not found in datasets directory.")
+    return pd.read_csv(filepath)

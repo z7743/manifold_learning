c76e2dc945117934660d651d51c0c6cd90ede786
z7743 <kapikapycapybara@gmail.com>
2024-06-20 21:03:46 +0900

    CCM migration

--- src/manifold_learning/__init__.py
+++ src/manifold_learning/__init__.py
@@ -1,6 +1,5 @@
 from .IMD import LinearProjectionNDim, IMD_nD , RandomTpRangeSubsetDataset
-from .CCM import FastCCM
 
 __all__ = [
-    'FastCCM', "LinearProjectionNDim", "IMD_nD", "RandomTpRangeSubsetDataset"
+     "LinearProjectionNDim", "IMD_nD", "RandomTpRangeSubsetDataset"
 ]
